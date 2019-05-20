0ecb322036dff012d63d2e0721015b7f2e0a0bc7
galmsx <mgalinovski@gmail.com>
2019-05-20 22:29:06 +0300

    del empty folder

--- controllers/reader/index.ts
+++
@@ -1,7 +0,0 @@
- import {ReaderService} from '../../services/ReaderService'
- export class ReaderController{
-     private readerService : ReaderService;
-     constructor(){
-         this.readerService = new ReaderService();
-     }
- }
